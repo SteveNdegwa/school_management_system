ee3d78b4d37acbdad0aa7d666a79d96876900e0f
SteveNdegwa <stevencallistus19@gmail.com>
2024-04-28 09:11:16 +0300

    add base models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,27 @@
+import uuid
+
 from django.db import models
 
 # Create your models here.
+
+
+class BaseModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract: True
+
+
+class GenericBaseModel(BaseModel):
+    name = models.CharField(max_length=50, null=True, blank=True)
+    description = models.CharField(max_length=50, null=True, blank=True)
+
+    class Meta:
+        abstract: True
+
+
+class State(GenericBaseModel):
+    def __str__(self):
+        return self.name
